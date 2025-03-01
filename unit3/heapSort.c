e9d772961f79a8100c1644e44a85e54ac8523afe
Vansh Tyagi <vanshjii021@gmail.com>
2025-03-01 06:47:34 +0530

    heap sort changes

--- unit3/heapSort.c
+++ unit3/heapSort.c
@@ -10,36 +10,36 @@ void swap(int* a, int* b) {
 // Function to heapify a subtree rooted at node i
 // n is the size of heap
 void heapify(int arr[], int n, int i) {
-    int largest = i;    // Initialize largest as root
+    int smallest = i;    // Initialize smallest as root
     int left = 2 * i + 1;    // Left child
     int right = 2 * i + 2;   // Right child
 
-    // If left child is larger than root
-    if (left < n && arr[left] > arr[largest])
-        largest = left;
+    // If left child is smaller than root
+    if (left < n && arr[left] < arr[smallest])
+        smallest = left;
 
-    // If right child is larger than largest so far
-    if (right < n && arr[right] > arr[largest])
-        largest = right;
+    // If right child is smaller than smallest so far
+    if (right < n && arr[right] < arr[smallest])
+        smallest = right;
 
-    // If largest is not root
-    if (largest != i) {
-        swap(&arr[i], &arr[largest]);
+    // If smallest is not root
+    if (smallest != i) {
+        swap(&arr[i], &arr[smallest]);
         // Recursively heapify the affected sub-tree
-        heapify(arr, n, largest);
+        heapify(arr, n, smallest);
     }
 }
 
 // Main function to perform heap sort
 void heapSort(int arr[], int n) {
-    // Build max heap
+    // Build min heap
     for (int i = n / 2 - 1; i >= 0; i--)
         heapify(arr, n, i);
 
     // Extract elements from heap one by one
     for (int i = n - 1; i > 0; i--) {
         swap(&arr[0], &arr[i]);    // Move current root to end
-        heapify(arr, i, 0);        // Call max heapify on reduced heap
+        heapify(arr, i, 0);        // Call min heapify on reduced heap
     }
 }
 
